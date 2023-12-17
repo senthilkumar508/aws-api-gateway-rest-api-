eb4c90a94a62d96035b990394c4948edcf0a9239
Senthilkumar <91686032+senthilkumar508@users.noreply.github.com>
2023-12-17 14:18:39 -0500

    Update lambda_authoriser.py
    update

--- lambda_authoriser.py
+++ lambda_authoriser.py
@@ -1,10 +1,16 @@
 def lambda_handler(event, context):
-    
+
+    #1 - Log the event
+    print('*********** The event is: ***************')
+    print(event)
+
+    #2 - See if the person's token is valid
     if event['authorizationToken'] == '1234':
         auth_status = 'Allow'
     else:
         auth_status = 'Deny'
-    
+
+    #3 - Construct and return the response
     authResponse = {
         'policyDocument': { 
             'Version': '2012-10-17',
@@ -12,7 +18,8 @@ def lambda_handler(event, context):
                 {
                     'Action': 'execute-api:Invoke',
                     'Resource': [
-                        event['methodArn']
+                        #event['methodArn']
+                        ["arn:aws:execute-api:us-east-1:YOURACCOUNTNUMBER:2ogoj2ul12/test/GET/customers"]
                     ], 
                     'Effect': auth_status
                 }
